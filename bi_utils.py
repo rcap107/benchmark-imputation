7ff2687d03eb153aac5e2a876c8a74b1b0ec9d43
cappuzzo <riccardo.cappuzzo@eurecom.fr>
2021-12-03 17:09:33 +0100

    Updated utils

--- bi_utils.py
+++ bi_utils.py
@@ -1,4 +1,8 @@
+import os.path as osp
+import os
+
 HOLOCLEAN_FOLDER='variants/holoclean'
+GRIMP_FOLDER='variants/grimp'
 CLEAN_DS_FOLDER='data/clean'
 DIRTY_DS_FOLDER='data/dirty'
 IMPUTED_DS_FOLDER='data/imputed'
